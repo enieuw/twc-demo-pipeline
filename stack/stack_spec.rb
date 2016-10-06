ad57c1440e3cb6a41f4b751374a469ec5d45c329
Eric Nieuwenhuijsen <enieuwenhuijsen@xebia.com>
2016-10-07 00:07:02 +0200

    Add curl test

--- stack/stack_spec.rb
+++ stack/stack_spec.rb
@@ -10,4 +10,8 @@ describe "Dockerfile" do
    describe file('/usr/share/nginx/html/index.html') do
       it { should contain '<h1>Hello world!</h1>' }
    end
+
+   describe command('curl 127.0.0.1') do
+      its(:stdout) { should contain '<h1>Hello world!</h1>' }
+   end
 end
