25dfd5fc195a15208d483af8dd5576077d7d7bea
Eric Nieuwenhuijsen <enieuwenhuijsen@xebia.com>
2016-10-07 13:05:03 +0200

    Only test the tdd folder

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -ti -e CI_CAPTURE=off -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/usr/src/app/spec enieuw/serverspec
+docker run -ti -e CI_CAPTURE=off -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/usr/src/app/spec enieuw/serverspec test["tdd"]
 
